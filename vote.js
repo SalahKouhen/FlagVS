f2bd8d21b84648af21ace9f853a64758a2e98325
Salah Kouhen <73072331+SalahKouhen@users.noreply.github.com>
2026-01-19 13:01:20 +0000

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,1 +1,8 @@
-
+fetch("https://flag-vs-api.salahkouhen.workers.dev/pair")
+  .then(res => res.json())
+  .then(data => {
+    console.log("PAIR FROM API:", data);
+  })
+  .catch(err => {
+    console.error("API ERROR:", err);
+  });
