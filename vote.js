d12c031ebe72acd71af15b1f994573ec534359e0
Salah Kouhen <73072331+SalahKouhen@users.noreply.github.com>
2026-01-19 13:25:10 +0000

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,8 +1,39 @@
+let currentPair = null;
+
 fetch("https://flag-vs-api.salahkouhen.workers.dev/pair")
   .then(res => res.json())
   .then(data => {
-    console.log("PAIR FROM API:", data);
-  })
-  .catch(err => {
-    console.error("API ERROR:", err);
+    currentPair = data;
+
+    document.getElementById("left-flag").textContent =
+      data.flagA.name;
+
+    document.getElementById("right-flag").textContent =
+      data.flagB.name;
   });
+
+document.getElementById("left-flag").onclick = () => {
+  submitVote("A");
+};
+
+document.getElementById("right-flag").onclick = () => {
+  submitVote("B");
+};
+
+function submitVote(choice) {
+  fetch("https://flag-vs-api.salahkouhen.workers.dev/vote", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      flagA: currentPair.flagA.code,
+      flagB: currentPair.flagB.code,
+      winner: choice
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log("Vote accepted:", data);
+    });
+}
